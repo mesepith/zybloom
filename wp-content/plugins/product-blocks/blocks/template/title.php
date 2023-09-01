55aab16846dc08842e3cfa4da34b8b49a34223d1
zahi <mesepith>
2023-09-01 22:26:15 +0530

    plugin deleted

--- wp-content/plugins/product-blocks/blocks/template/title.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('ABSPATH') || exit;
-
-$title_data .= '<'.esc_attr($attr['titleTag']).' class="wopb-block-title"><a href="'.esc_url($titlelink).'">'.$title.'</a></'.esc_attr($attr['titleTag']).'>';
